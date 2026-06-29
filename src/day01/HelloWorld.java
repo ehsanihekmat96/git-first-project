f44b20c886fb7bcdbfd868d494b88aa0a5cf9880
Hekmat <ehsani.hekmat96@gmail.com>
2026-06-29 12:08:30 -0700

    The new code adde for both class

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -2,7 +2,7 @@ package day01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello worldd");
         System.out.println("This is the first class has been created till now");
     }
 }
--- src/day01/SecondExample.java
+++ src/day01/SecondExample.java
@@ -2,6 +2,6 @@ package day01;
 
 public class SecondExample {
     public static void main(String[] args) {
-        System.out.println("This is the second class has been created");
+        System.out.println("This is the secondd class has been created");
     }
 }
