0595cfd618a4ed501124ca87396c92c625a1ba0e
ehsanihekmat96 <ehsani.hekmat96@gmail.com>
2026-06-29 13:05:29 -0700

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,8 +3,6 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello worldd");
-        System.out.println("This is the first class has been created till now");
-        System.out.println("We are ready for the new lesson and you should get reae");
-        System.out.println("We are ready for the new lesson and this is the 3rd lesson");
+     
     }
 }
