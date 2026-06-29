0201bca9d827d961e6ef4260e03a0a7a76010bb9
ehsanihekmat96 <ehsani.hekmat96@gmail.com>
2026-06-29 13:05:06 -0700

    Update SecondExample.java

--- src/day01/SecondExample.java
+++ src/day01/SecondExample.java
@@ -2,8 +2,7 @@ package day01;
 
 public class SecondExample {
     public static void main(String[] args) {
-        System.out.println("This is the secondd class has been created");
-        System.out.println("This is not the our main class and we need to have the 3rd class");
-        // some new changes added to this class;
+        System.out.println("Hello my Friend");
+        
     }
 }
